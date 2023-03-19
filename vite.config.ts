133e1edefe3bcefecc2440927bf1221ec7b93099
Igor Antonenko <i_antonenko@lesta.group>
2023-03-19 05:00:08 +0300

    feat: add veriables

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/abstract/variables";`,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
